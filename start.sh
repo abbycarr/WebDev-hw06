9f398ababcab5aaf022c51d63c25312b62c7a950
abbycarr <carr.ab@husky.neu.edu>
2021-02-23 17:58:39 -0500

    deploy/start scripts
    Co-authored-by: Jehane Ali <jehaneali@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
-# #!/bin/bash
+#!/bin/bash
 
-# export MIX_ENV=prod
-# export PORT=4801
+export MIX_ENV=prod
+export PORT=4802
 
-# CFGD=$(readlink -f ~/.config/bulls)
+CFGD=$(readlink -f ~/.config/multi_bull)
 
-# if [ ! -e "$CFGD/base" ]; then
-#     echo "run deploy first"
-#     exit 1
-# fi
+if [ ! -e "$CFGD/base" ]; then
+    echo "run deploy first"
+    exit 1
+fi
 
-# SECRET_KEY_BASE=$(cat "$CFGD/base")
-# export SECRET_KEY_BASE
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
 
-# _build/prod/rel/bulls/bin/bulls start
+_build/prod/rel/multi_bull/bin/multi_bull start
